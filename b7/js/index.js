9fab71c69c38ca547851ffa8ef610fa67de2ffb6
nguyenmanhdung3007 <nguyenmanhdung3007@gmail.com>
2023-05-27 18:08:04 +0700

    homework

---
+++ b7/js/index.js
@@ -0,0 +1,33 @@
+console.log("Hello World");
+
+// Bai1
+// let user = prompt("Hay nhap ten cua ban",);
+
+// console.log(user)
+
+// bai2
+// let yourName=prompt("Hay nhap ten cua ban:",);
+// let nameUpperCase=yourName.toUpperCase();
+// alert("Ten cua ban la: "+ nameUpperCase);
+
+
+// Bai3
+// // let firstName=prompt("Hay nhap ho cua ban:",);
+// // let lastName=prompt("Hay nhap ten cua ban:",);
+
+// alert("Hello "+firstName+" "+lastName);
+
+// Bai4
+// let number1=prompt("So thu nhat la:");
+// let number2=prompt("So thu hai la:");
+// let sum=Number(number1)+ Number(number2);
+
+// console.log(sum)
+// alert("Tong cua 2 so vua nhap la: " + sum.toString())
+ 
+
+// bai5
+// let number=Number(prompt("So can tinh la: "));
+// let number1 = number % 2;
+// let number2 = number / 2;
+// alert("So "+number+" chia cho 2 duoc "+ number2+" du " + number1);
